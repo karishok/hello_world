d1a7a829c1d63b3917c9ae5e475e091d0c7145d5
karishok <babaliants@bk.ru>
2012-06-22 23:24:06 +0400

    Update master

--- delpre.php
+++ delpre.php
@@ -2,6 +2,9 @@
         include_once('misc.php');
         $predmet= $_GET['predmet'];
 		$db = connectdb();
+		
+		
+		
 		$query= "DELETE FROM pred where predmet='$predmet'";
 		$res = mysql_query($query, $db);
 		header('Location:pre.php');
