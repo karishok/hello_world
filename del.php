928d172528c2244587312dbe414ecf4b5d194296
karishok <babaliants@bk.ru>
2012-06-22 23:42:30 +0400

    Update master

--- del.php
+++ del.php
@@ -4,6 +4,7 @@
         $id= $_GET['id'];
 		$pre= $_GET['pre'];
 		$db = connectdb();
+		
 		$query= "DELETE FROM zadanie where id='$id'";
 		$res = mysql_query($query, $db);
 		header("Location:pre.php?pre=$pre");
